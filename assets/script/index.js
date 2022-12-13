2f4cc72ed5b4534608f8a21a6d9ed356533c71e3
brendantyler <galmanbrendan@gmail.com>
2022-12-13 10:27:06 -0600

    add gps

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,18 +1,48 @@
 const loadDiv = document.getElementById('overlay')
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnJlbmRhbmdhbG1hbiIsImEiOiJja3hkaDY3ejMwZDhoMnZwZXByMDV2enQ5In0.hHhzrbbSrPpNQbtmaO2b-g';
+
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: [-51.92528, -14.235004],
-  zoom: 5
+  pitch: 40,
+  zoom: 16
 });
 
-function getUserLocation() {
-  if (navigator.geolocation) {
-    
-    navigator.geolocation.getCurrentPosition(showPosition, showError);
-  } else { 
-    locationResponse.innerHTML = "Geolocation is not supported by this browser.";
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
+
+const marker = new mapboxgl.Marker({
+  color: '3898ff'
+});
+
+function getLocation(position){
+  const {longitude, latitude} = position.coords;
+
+  if (longitude && latitude) {
+    map.setCenter([longitude,latitude])
+    marker.setLngLat([longitude,latitude]).addTo(map);
+    setTimeout(() => {overlay.style.display = 'none'}, 750)
   }
+}
+
+function errorHandler(event) {
+  loading.style.animationPlayState = 'paused';
+  console.log(event.message);
+}
+
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 0
+};
+
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(getLocation, errorHandler, options);
+} else { 
+  console.log('geolocation is not supported by your browser')
 }
